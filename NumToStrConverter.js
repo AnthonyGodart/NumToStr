17eec263e1d13f318c1bd2e0944a32e9b92df40c
Anthony GODART <107860091+AnthonyGodart@users.noreply.github.com>
2023-12-27 11:04:50 +0100

    updated ReadMe and code

--- NumToStrConverter.js
+++ NumToStrConverter.js
@@ -1,7 +1,17 @@
+/**
+ * Convertit un nombre en lettres.
+ * Fonction principale à utiliser comme macro dans Google Sheets.
+ *
+ * @param {number} number - Le nombre à convertir.
+ * @returns {string} - La représentation en lettres du nombre.
+ */
+
 const UNITS = ['', 'Un', 'Deux', 'Trois', 'Quatre', 'Cinq', 'Six', 'Sept', 'Huit', 'Neuf'];
 const TEENS = ['Dix', 'Onze', 'Douze', 'Treize', 'Quatorze', 'Quinze', 'Seize', 'Dix-Sept', 'Dix-Huit', 'Dix-Neuf'];
 const TENS = ['', '', 'Vingt', 'Trente', 'Quarante', 'Cinquante', 'Soixante', 'Soixante-', 'Quatre-', 'Quatre-Vingt'];
 
+
+/* Main function */ 
 function convertNumToStr(number) {
     if (number === 0) {
         return 'Zero';
@@ -15,53 +25,70 @@ function convertNumToStr(number) {
 
     if (postComma) {
         result += ' Euro et ' + postComma + (postComma === 'Un' ? ' Cent' : ' Cents');
+    } else {
+        result += ' Euro';
     }
 
     return result;
 }
 
+/* Internal functions */ 
 function convertPreCommaToStr(preComma) {
     if (preComma < 10) {
-        return UNITS[preComma];
+        return convertUnits(preComma);
     } else if (preComma < 20) {
-        return TEENS[preComma - 10];
+        return convertTeens(preComma);
     } else if (preComma < 100) {
-      const ten = Math.floor(preComma / 10);
-      const unity = preComma % 10;
-
-      if (ten === 7) {
-        // Pour 70 à 79, utilisez 'Soixante-Dix'
-        return 'Soixante' + (unity !== 0 ? '-' + TEENS[unity] : '-Dix');
-      } else if (ten === 8) {
-        // Pour 80 à 89, utilisez 'Quatre-Vingt'
-        return 'Quatre-Vingt' + (unity === 0 ? '' : (unity === 10 ? 'Dix' : '-' + UNITS[unity]));
-      } else if (ten === 9) {
-        // Pour 90 à 99, utilisez 'Quatre-Vingt-Dix'
-        return 'Quatre-Vingt' + (unity !== 0 ? '-' + TEENS[unity] : '-Dix');
-      } else {
-        // Pour les autres dizaines
-        return (TENS[ten] + (unity !== 0 ? '-' + UNITS[unity] : '')) || UNITS[unity];
-      }
-    }
-    else if (preComma < 1000) {
-        const hundred = Math.floor(preComma / 100);
-        const remainder = preComma % 100;
-        return (hundred > 1 ? UNITS[hundred] : '') + ' Cent' + (remainder !== 0 ? ' ' + convertPreCommaToStr(remainder) : '');
-    } else if (preComma < 1000000) {
-        const thousand = Math.floor(preComma / 1000);
-        const remainder = preComma % 1000;
-        return (thousand > 1 ? convertPreCommaToStr(thousand) : '') + ' Mille' + (remainder !== 0 ? ' ' + convertPreCommaToStr(remainder) : '');
-    } else if (preComma < 1000000000) {
-        const million = Math.floor(preComma / 1000000);
-        const remainder = preComma % 1000000;
-        return convertPreCommaToStr(million) + (remainder > 1 ? ' Millions' : ' Million' ) + (remainder !== 0 ? ' ' + convertPreCommaToStr(remainder) : '');
-    } else if (preComma < 1000000000000) {
-        const billion = Math.floor(preComma / 1000000000);
-        const remainder = preComma % 1000000000;
-        return convertPreCommaToStr(billion) + (remainder > 1 ? ' Milliards' : ' Milliard' ) + (remainder !== 0 ? ' ' + convertPreCommaToStr(remainder) : '');
+        return convertTens(preComma);
+    } else if (preComma < 1000) {
+        return convertHundreds(preComma);
     } else {
-        return 'Number not supported';
+        return convertLargeNumbers(preComma);
     }
+}
+
+function convertUnits(value) {
+    return UNITS[value];
+}
+
+function convertTeens(value) {
+    return TEENS[value - 10];
+}
+
+function convertTens(value) {
+    const ten = Math.floor(value / 10);
+    const unity = value % 10;
+
+    switch (ten) {
+        case 7:
+            return 'Soixante' + (unity !== 0 ? '-' + TEENS[unity] : '-Dix');
+        case 8:
+            return 'Quatre-Vingt' + (unity === 0 ? '' : (unity === 10 ? 'Dix' : '-' + UNITS[unity]));
+        case 9:
+            return 'Quatre-Vingt' + (unity !== 0 ? '-' + TEENS[unity] : '-Dix');
+        default:
+            return (TENS[ten] + (unity !== 0 ? '-' + UNITS[unity] : '')) || UNITS[unity];
+    }
+}
+
+function convertHundreds(value) {
+    const hundred = Math.floor(value / 100);
+    const remainder = value % 100;
+    return (hundred > 1 ? UNITS[hundred] + ' Cent' : 'Cent') + (remainder !== 0 ? ' ' + convertPreCommaToStr(remainder) : '');
+}
+
+function convertLargeNumbers(value) {
+    const suffixes = ['', 'Mille', 'Million', 'Milliard'];
+    for (let i = 3; i <= 12; i += 3) {
+        const divisor = Math.pow(10, i);
+        if (value < divisor) {
+            const quotient = Math.floor(value / (divisor / 1000));
+            const remainder = value % (divisor / 1000);
+            const suffix = quotient > 1 ? suffixes[i / 3] + 's' : suffixes[i / 3];
+            return convertPreCommaToStr(quotient) + ' ' + suffix + (remainder !== 0 ? ' ' + convertPreCommaToStr(remainder) : '');
+        }
+    }
+    return 'Number not supported';
 }
 
 function convertPostCommaToStr(postComma) {
@@ -73,13 +100,14 @@ function convertPostCommaToStr(postComma) {
     }
 }
 
-/* Tests */
+/* EXEMPLES D'UTILISATION */
+/* Un-comment wished line */
 //const numbers = [12370, 12371, 12372, 12373, 12374, 12375, 12376, 12377, 12378, 12379];
 //const numbers = [12380, 12381, 12382, 12383, 12384, 12385, 12386, 12387, 12388, 12389];
-//const numbers = [12390, 12391, 12392, 12393, 12394, 12395, 12396, 12397, 12398, 12399];
+const numbers = [12390, 12391, 12392, 12393, 12394, 12395, 12396, 12397, 12398, 12399];
 //const numbers = [70000, 71000, 78000, 80000, 81000, 88000, 90000, 91000, 98000];
-const numbers = [34.01, 34.02]
-numbers.map(number => {
+//const numbers = [34.01, 34.02]
+numbers.forEach(number => {
   const answer= convertNumToStr(number);
   console.log(number)
   console.log(answer)
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# NumToStr
+# Numeric to String Converter ( French Only )
+
+I created this code originally for Google Sheet use, as a macro to convert a numeric number to its string version.
+
+Supported language is french only as this was the only one I needed.
+
+
+## How to use guide
+
+- Open your sheet with @https://docs.google.com/spreadsheets/create?addon_store
+
+- Go on Extension menu > Apps Script
+
+- Replace "my_function() {}" with this repo's .js code, rename and save
+
+- Go back on your sheet
+
+- Go again on Extension menu > Macros > Import Macro and click on "convertNumToStr" : Add function.
+
+You can now type "=convertNumToStr(-SelectedCell-)" in the cell you want to use this function.
